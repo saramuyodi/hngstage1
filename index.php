639c406589c0abae5fad615c96ea6c11e713190a
SARAH MUYODI <38284621+saramuyodi@users.noreply.github.com>
2018-04-18 05:22:03 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="UTF-8">
+	<head>
+  	<title>HNG INTERNSHIP 4.O</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body style="width:100%; text-align:center; 
+background-color:lime;">
+
+<div>
+<a href="#">
+<img src="home.png" width="128px" height="128px"/></a>
+</div>
+
+<div style="padding: 2px;text-align: center; 
+font-family: tahoma;font-size: 93%; color: #000;">
+<h2>WELCOME TO HNG INTERNSHIP 4.O 
+ </h2>
+
+<?php
+     print date("m/d/y G.i:s<br>", time());
+   
+   
+?>
+ 
+
+</div>
+
+	
+
+
+		
+					
+</body>
+</html>	
